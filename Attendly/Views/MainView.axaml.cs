1600aa1be62b086c8dd1a26ba5d83b1861aa93b1
Salty Duck <142966149+Megaloduck@users.noreply.github.com>
2026-07-28 12:56:58 +0700

    Add project files.

---
+++ Attendly/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Attendly.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Attendly/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Attendly.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
