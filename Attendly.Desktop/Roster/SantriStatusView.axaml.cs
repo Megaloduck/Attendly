0891b5d70eb0830abf7851a06d07b35e6fc7e3c3
Salty Duck <142966149+Megaloduck@users.noreply.github.com>
2026-08-13 01:31:47 +0700

    try dashboard refine

---
+++ Attendly.Desktop/Roster/SantriStatusView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Attendly.Desktop;
+
+public partial class SantriStatusView : UserControl
+{
+    public SantriStatusView()
+    {
+        InitializeComponent();
+    }
+}
